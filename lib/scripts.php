30db59b425a102d9a97df0ead0980ed3dbf408a8
Philipp Steinweber <p@steinweber.net>
2013-11-20 12:04:39 +0700

    margins, thumbnail position

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c2a1283ffdfd6002555861a88137eddd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '485871b82648250a495b05ac05942b26');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content.php
+++ templates/content.php
@@ -4,13 +4,9 @@
   </header>
   <div class="entry-summary">
     <div class="row">
-        <div class="col-sm-8"><?php the_excerpt(); ?></div>
-        <div class="col-sm-4"> <?php if ( has_post_thumbnail()) : ?>
-   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-   <?php the_post_thumbnail('thumbnail'); ?>
-   </a>
- <?php endif; ?></div>
+        <div class="col-sm-3"> <?php if ( has_post_thumbnail()) : ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail('thumbnail'); ?></a><?php endif; ?></div>
+        <div class="col-sm-9"><?php the_excerpt(); ?><?php get_template_part('templates/entry-meta'); ?></div>
     </div>
-    <?php get_template_part('templates/entry-meta'); ?>
+    
   </div>
 </article>
